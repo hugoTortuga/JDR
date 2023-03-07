af4bf8201d1cfa07e18ddf56f748ff486e50188d
El_Tortuga <hugo.malglaive@gmail.com>
2023-03-07 22:39:31 +0100

    ajouter tests

--- JDR.Test/Dice.cs
+++ JDR.Test/Dice.cs
@@ -2,7 +2,7 @@ using JDR.Modele;
 
 namespace JDR.Test
 {
-    public class Tests
+    public class DiceTest
     {
         [SetUp]
         public void Setup()
@@ -27,5 +27,25 @@ namespace JDR.Test
         {
             Assert.Throws(typeof(ArgumentException), () => new Dice(0));
         }
-    }
+
+		[Test]
+        [Repeat(100)]
+		public void RollDice20between1and20() {
+			var result = new Dice(20).Roll();
+            Assert.That(result, Is.GreaterThanOrEqualTo(1));
+			Assert.That(result, Is.AtMost(20));
+		}
+
+		[Test]
+        [TestCase(10)]
+		[TestCase(20)]
+		[TestCase(50)]
+		[TestCase(100)]
+		public void RollDiceBetween1andNumberOfFace(int numberOfFace) {
+			var result = new Dice(numberOfFace).Roll();
+			Assert.That(result, Is.GreaterThanOrEqualTo(1));
+			Assert.That(result, Is.AtMost(numberOfFace));
+		}
+
+	}
 }
