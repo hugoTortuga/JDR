0b48e16197b36c9e62b2a47b550fee94324382b5
El_Tortuga <hugo.malglaive@gmail.com>
2023-04-01 17:22:40 +0200

    elipse + changement de background

--- JDR.Vue/Views/UCGame.xaml.cs
+++ JDR.Vue/Views/UCGame.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -21,8 +22,9 @@ namespace JDR.Vue.Views {
 	/// </summary>
 	public partial class UCGame : UserControl {
 
-		private bool _isPlayerSelected;
-		private Rectangle _selectionRectangle;
+		private bool _IsPlayerSelected;
+		private Ellipse _SelectionForm;
+		private int _FormSize = 16;
 
 		public UCGame(MainWindow window) {
 			InitializeComponent();
@@ -33,50 +35,55 @@ namespace JDR.Vue.Views {
 		}
 
 		private void PlayerClicked(object sender, EventArgs e) {
-			if (_isPlayerSelected) {
-				_isPlayerSelected = false;
-				if (_selectionRectangle != null) {
-					GameCanvas.Children.Remove(_selectionRectangle);
-					_selectionRectangle = null;
+			if (_IsPlayerSelected) {
+				_IsPlayerSelected = false;
+				if (_SelectionForm != null) {
+					GameCanvas.Children.Remove(_SelectionForm);
+					_SelectionForm = null;
 				}
 			}
 			else {
 				// Sélectionner le joueur et dessiner le carré de sélection
-				_isPlayerSelected = true;
-				_selectionRectangle = new Rectangle {
-					Width = Player1.ActualWidth + 4,
-					Height = Player1.ActualHeight + 4,
-					Stroke = Brushes.Red,
-					StrokeThickness = 2
+				
+				_IsPlayerSelected = true;
+				_SelectionForm = new Ellipse {
+					Width = Player1.ActualWidth + _FormSize,
+					Height = Player1.ActualHeight + _FormSize,
+					Stroke = Brushes.Black,
+					StrokeThickness = 2,
+					Opacity = 0.8
 				};
 
-				Canvas.SetLeft(_selectionRectangle, Canvas.GetLeft(Player1) - 2);
-				Canvas.SetTop(_selectionRectangle, Canvas.GetTop(Player1) - 2);
+				Canvas.SetLeft(_SelectionForm, Canvas.GetLeft(Player1) - _FormSize / 2);
+				Canvas.SetTop(_SelectionForm, Canvas.GetTop(Player1) - _FormSize / 2);
 
-				GameCanvas.Children.Add(_selectionRectangle);
+				GameCanvas.Children.Add(_SelectionForm);
 			}
 		}
 
 		private void GameCanvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e) {
-			if (_isPlayerSelected) {
+			if (_IsPlayerSelected) {
 				var newMousePosition = e.GetPosition(GameCanvas);
 
 				Canvas.SetLeft(Player1, newMousePosition.X - (Player1.Width / 2));
 				Canvas.SetTop(Player1, newMousePosition.Y - (Player1.Height / 2));
 
-				Canvas.SetLeft(_selectionRectangle, newMousePosition.X - (Player1.Width / 2));
-				Canvas.SetTop(_selectionRectangle, newMousePosition.Y - (Player1.Height / 2));
+				Canvas.SetLeft(_SelectionForm, newMousePosition.X - (Player1.Width / 2) - (_FormSize / 2));
+				Canvas.SetTop(_SelectionForm, newMousePosition.Y - (Player1.Height / 2) - (_FormSize / 2));
 			}
 		}
 
-		private void GameCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
-			
-		}
 
-		private void GameCanvas_MouseMove(object sender, MouseEventArgs e) {
-			if (_isPlayerSelected && GameCanvas.IsMouseCaptured) {
-				
+		private void ChangeMap(object sender, RoutedEventArgs e) {
+			var dialog = new OpenFileDialog();
+			if (dialog.ShowDialog() == true) {
+				var image = new ImageBrush(new BitmapImage(new Uri(dialog.FileName)));
+				image.Stretch = Stretch.UniformToFill;
+				GameCanvas.Background = image;
+
+
 			}
+
 		}
 	}
 }
