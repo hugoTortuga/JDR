f6c1acb2fd0cab0aef1bc2ce06ab5d0196cf7d45
el-tortuga <hugo.malglaive@gmail.com>
2023-04-17 22:19:09 +0200

    commit before test custom polygons

--- JDR.Vue/Utils/Polygon.cs
+++ JDR.Vue/Utils/Polygon.cs
@@ -1,23 +1,37 @@
-﻿using System;
+﻿using JDR.Vue.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 
 namespace JDR.Vue.Utils
 {
-    public class Polygon : UIElement
+    public class CustomPolygon : ViewModelBase
     {
+        private PointCollection _Points;
+        private bool _IsSelected;
 
-        public static readonly DependencyProperty IsSelectedProperty =
-        DependencyProperty.Register(nameof(IsSelected), typeof(bool), typeof(Polygon), new PropertyMetadata(false));
+        public PointCollection Points
+        {
+            get { return _Points; }
+            set
+            {
+                _Points = value;
+                OnPropertyChanged(nameof(Points));
+            }
+        }
 
         public bool IsSelected
         {
-            get { return (bool)GetValue(IsSelectedProperty); }
-            set { SetValue(IsSelectedProperty, value); }
+            get { return _IsSelected; }
+            set
+            {
+                _IsSelected = value;
+                OnPropertyChanged(nameof(IsSelected));
+            }
         }
-
     }
 }
